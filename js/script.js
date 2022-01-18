05bc36f2149d57fff4ddad904f2cc5df24a805ca
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2022-01-18 14:06:04 +0400

    Added search icon and fixed List Scroll

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 $(document).ready(() => {
-    var myContainerHeight = $("#MyContainer").outerHeight();
-    var chatHeadHeight = $("#menu .chat-header").outerHeight() + $("#search-block").outerHeight();
-    $("#chat-list").css("height", myContainerHeight-chatHeadHeight);
-    console.log(chatHeadHeight);
+  Adjust();
+  $(window).on("resize", () => {
+    Adjust();
+  });
+});
 
-})
+const Adjust = () => {
+  var myContainerHeight = $("#MyContainer").outerHeight();
+  var chatHeadHeight =$("#menu .chat-header").outerHeight()+$("#search-section").outerHeight();
+  $("#chat-list").css("height", myContainerHeight - chatHeadHeight);
+  console.log(chatHeadHeight);
+};
