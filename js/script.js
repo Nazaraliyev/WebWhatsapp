7f70db5cb818de16449f50b59b60d8f643432a79
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2022-01-18 16:52:21 +0400

    Modified js document for chat page scroll

--- js/script.js
+++ js/script.js
@@ -6,8 +6,13 @@ $(document).ready(() => {
 });
 
 const Adjust = () => {
+  //Menu
   var myContainerHeight = $("#MyContainer").outerHeight();
   var chatHeadHeight =$("#menu .chat-header").outerHeight()+$("#search-section").outerHeight();
+  
+  // Chat
+  var chatMessagesOutHeight = $("#chat-profile-info-wrapper").outerHeight()+$("#message-input-wrapper").outerHeight();
+  
   $("#chat-list").css("height", myContainerHeight - chatHeadHeight);
-  console.log(chatHeadHeight);
+  $("#chat-messages-container").css("height", myContainerHeight - chatMessagesOutHeight);
 };
